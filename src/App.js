9a7bd5246919bfe8aab62c1f017fc30eb5f40897
Elizabeth Lee <elizabethlee245@gmail.com>
2022-10-24 16:24:51 -0400

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import ProductCard from './views/home/productCard';
 
 function App() {
   return (
+    /* (2) Update App.js to only return the homepage component. */
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <ProductCard />
+        
       </header>
     </div>
   );
 }
 
 export default App;
+
+//only return the homepage component from index.js
---
+++ src/views/home/index.js
@@ -0,0 +1,14 @@
+// Create a React component for the homepage
+
+import React, { Component } from 'react';
+//import './HomePage.css';
+
+class HomePage extends Component {
+  render() {
+    return (
+ 
+    );
+  }
+}
+
+export default HomePage
